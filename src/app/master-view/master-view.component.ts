df2b0c71d38fd83f41faef334851b814be99c409
Zdravko Kolev <zdrawko.kolew@gmail.com>
2024-06-03 11:02:07 +0300

    untitled-app (9) updates

---
+++ src/app/master-view/master-view.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { IGX_GRID_DIRECTIVES } from '@infragistics/igniteui-angular';
+import { Subject, takeUntil } from 'rxjs';
+import { EmployeesType } from '../models/northwind/employees-type';
+import { NorthwindService } from '../services/northwind.service';
+
+@Component({
+  selector: 'app-master-view',
+  standalone: true,
+  imports: [IGX_GRID_DIRECTIVES],
+  templateUrl: './master-view.component.html',
+  styleUrls: ['./master-view.component.scss']
+})
+export class MasterViewComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject<void>();
+  public northwindEmployees: EmployeesType[] = [];
+
+  constructor(
+    private northwindService: NorthwindService,
+  ) {}
+
+  ngOnInit() {
+    this.northwindService.getEmployees().pipe(takeUntil(this.destroy$)).subscribe({
+      next: (data) => this.northwindEmployees = data,
+      error: (_err: any) => this.northwindEmployees = []
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
