979d8c9586fce7428e508295cd2499faccc611e5
Zdravko Kolev <zkolev@infragistics.com>
2024-06-19 09:03:02 +0300

    Add non-grid remote pagination

--- src/app/master-view/master-view.component.ts
+++ src/app/master-view/master-view.component.ts
@@ -1,5 +1,11 @@
 import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
-import { GridPagingMode, IGX_GRID_DIRECTIVES, IgxGridComponent } from '@infragistics/igniteui-angular';
+import { GridPagingMode, IGX_GRID_DIRECTIVES, IgxGridComponent, IgxPaginatorComponent, IgxCardComponent,
+  IgxRippleDirective, IgxButtonDirective, IgxIconModule, IgxInputGroupComponent,
+  IgxCardActionsComponent,
+  IgxCardMediaDirective,
+  IgxCardHeaderComponent,
+  IgxCardContentDirective
+ } from '@infragistics/igniteui-angular';
 import { Observable } from 'rxjs';
 import { NorthwindService } from '../services/northwind.service';
 import { CommonModule } from '@angular/common';
@@ -7,19 +13,24 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-master-view',
   standalone: true,
-  imports: [IGX_GRID_DIRECTIVES, CommonModule],
+  imports: [IGX_GRID_DIRECTIVES, CommonModule, IgxIconModule, IgxInputGroupComponent, IgxCardComponent, IgxCardActionsComponent, 
+    IgxCardMediaDirective, IgxCardHeaderComponent, IgxCardContentDirective, IgxPaginatorComponent, IgxRippleDirective, IgxButtonDirective],
   templateUrl: './master-view.component.html',
   styleUrls: ['./master-view.component.scss']
 })
 export class MasterViewComponent implements AfterViewInit {
   @ViewChild('grid1', { static: true }) public grid1: IgxGridComponent;
   @ViewChild('customPager', { read: TemplateRef, static: true }) public remotePager: TemplateRef<any>;
+  @ViewChild('paginator', { static: true }) public paginator!: IgxPaginatorComponent;
   public totalRecords: Observable<number>;
   public data: Observable<any[]>;
   public mode = GridPagingMode.Remote;
   public isLoading = true;  
   private _perPage = 15;
+  private _cardsPerPage = 3;
+  public cardItemsPerPage = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30];
 
+  // For Grid
   public get perPage(): number {
     return this._perPage;
   }
@@ -27,6 +38,16 @@ export class MasterViewComponent implements AfterViewInit {
   public set perPage(val: number) {
     this._perPage = val;
     this.paginate();
+  }
+
+  // For cards
+  public get perPageCards(): number {
+    return this._cardsPerPage;
+  }
+
+  public set perPageCards(val: number) {
+    this._cardsPerPage = val;
+    this.paginateCards();
   }
 
   constructor(private northwindService: NorthwindService) { }
@@ -53,11 +74,22 @@ export class MasterViewComponent implements AfterViewInit {
     this.northwindService.getData(skip, top);
   }
 
+  public paginateCards() {
+    const { skip, top } = this.calculateCardsPagination();
+
+    this.northwindService.getData(skip, top);
+  }
+  
   // Later can be extended to support OrderBy query option or even sorting and filtering
   private calculatePagination() {
-    debugger;
     const skip = this.grid1.page * this._perPage;
     const top = this._perPage;
+    return { skip, top };
+  }
+
+  private calculateCardsPagination() {
+    const skip = this.paginator.page * this._cardsPerPage;
+    const top = this._cardsPerPage;
     return { skip, top };
   }
 }
