e98f3a1545b499ff1ffbc6692ca0091348e31dc2
zkolev <zkolev@infragistics.com>
2024-06-19 11:49:18 +0300

    Polishing code

--- src/app/master-view/master-view.component.ts
+++ src/app/master-view/master-view.component.ts
@@ -1,37 +1,37 @@
-import { AfterViewInit, Component, TemplateRef, ViewChild } from '@angular/core';
-import { GridPagingMode, IGX_GRID_DIRECTIVES, IgxGridComponent, IgxPaginatorComponent, IgxCardComponent,
+import { AfterViewInit, Component, OnDestroy, TemplateRef, ViewChild } from '@angular/core';
+import {
+  GridPagingMode, IGX_GRID_DIRECTIVES, IgxGridComponent, IgxPaginatorComponent, IgxCardComponent,
   IgxRippleDirective, IgxButtonDirective, IgxIconModule, IgxInputGroupComponent, IgxCheckboxComponent,
-  IgxCardActionsComponent,
-  IgxCardMediaDirective,
-  IgxCardHeaderComponent,
-  IgxCardContentDirective
- } from '@infragistics/igniteui-angular';
+  IgxCardActionsComponent, IgxCardMediaDirective, IgxCardHeaderComponent, IgxCardContentDirective
+} from '@infragistics/igniteui-angular';
 import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { NorthwindService } from '../services/northwind.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-master-view',
   standalone: true,
-  imports: [IGX_GRID_DIRECTIVES, IGX_GRID_DIRECTIVES, CommonModule, IgxIconModule, IgxInputGroupComponent, IgxCardComponent, IgxCardActionsComponent, 
-    IgxCardMediaDirective, IgxCardHeaderComponent, IgxCardContentDirective, IgxPaginatorComponent, IgxRippleDirective, IgxButtonDirective, IgxCheckboxComponent],
+  imports: [IGX_GRID_DIRECTIVES, CommonModule, IgxIconModule, IgxInputGroupComponent, IgxCardComponent,
+    IgxCardActionsComponent, IgxCardMediaDirective, IgxCardHeaderComponent, IgxCardContentDirective,
+    IgxPaginatorComponent, IgxRippleDirective, IgxButtonDirective, IgxCheckboxComponent],
   templateUrl: './master-view.component.html',
   styleUrls: ['./master-view.component.scss']
 })
-export class MasterViewComponent implements AfterViewInit {
-  private destroy$: Subject<void> = new Subject<void>();
+export class MasterViewComponent implements AfterViewInit, OnDestroy {
+  private destroy$ = new Subject<void>();
   @ViewChild('grid1', { static: true }) public grid1: IgxGridComponent;
   @ViewChild('customPager', { read: TemplateRef, static: true }) public remotePager: TemplateRef<any>;
   @ViewChild('paginator', { static: true }) public paginator!: IgxPaginatorComponent;
-  public data: Observable<any[]>;
-  public totalRecords: Observable<number>;
-  public cardsData: Observable<any[]>;
-  public totalCardsRecords: Observable<number>;
+  public data$: Observable<any[]>;
+  public totalRecords$: Observable<number>;
+  public cardsData$: Observable<any[]>;
+  public totalCardsRecords$: Observable<number>;
   public mode = GridPagingMode.Remote;
-  public isLoading = true;  
+  public isLoading = true;
   private _perPage = 15;
 
-  // Variables needed for Cards pagination
+  // Variables/AB properties needed for Cards pagination
   private _cardsPerPage = 5;
   public cardItemsPerPage = [5, 10, 15, 25, 50, 100, 500];
 
@@ -60,49 +60,43 @@ export class MasterViewComponent implements AfterViewInit {
   public ngAfterViewInit() {
     // Initialize Grid
     this.grid1.isLoading = true;
-    this.data = this.northwindService.remoteData.asObservable();
-    this.totalRecords = this.northwindService.dataLenght.asObservable();
+    this.data$ = this.northwindService.remoteData$;
+    this.totalRecords$ = this.northwindService.dataLength$;
 
     // Load Grid data
-    const { skip, top } = this.calculatePagination();
-    this.northwindService.getData(skip, top);
+    this.paginateGrid();
 
-    this.data.subscribe(() => {
+    this.data$.pipe(takeUntil(this.destroy$)).subscribe(() => {
       this.isLoading = false;
     });
 
     // Initialize Card data
-    this.cardsData = this.northwindService.remoteCardsData.asObservable();
-    this.totalCardsRecords = this.northwindService.dataCardsLenght.asObservable();
-    
+    this.cardsData$ = this.northwindService.remoteCardsData$;
+    this.totalCardsRecords$ = this.northwindService.dataCardsLength$;
+
     // Load Card data
-    const { skipCards, topCards } = this.calculateCardsPagination();
-    this.northwindService.getCardsData(skipCards, topCards);
+    this.paginateCards();
   }
 
   public paginateGrid() {
     this.isLoading = true;
-    const { skip, top } = this.calculatePagination();
-
+    const { skip, top } = this.calculatePagination(this.grid1.page, this._perPage);
     this.northwindService.getData(skip, top);
   }
 
   public paginateCards() {
-    const { skipCards, topCards } = this.calculateCardsPagination();
-
-    this.northwindService.getCardsData(skipCards, topCards);
+    const { skip, top } = this.calculatePagination(this.paginator.page, this._cardsPerPage);
+    this.northwindService.getCardsData(skip, top);
   }
-  
-  // Later can be extended to support OrderBy query option or even sorting and filtering
-  private calculatePagination() {
-    const skip = this.grid1.page * this._perPage;
-    const top = this._perPage;
+
+  private calculatePagination(page: number, perPage: number) {
+    const skip = page * perPage;
+    const top = perPage;
     return { skip, top };
   }
 
-  private calculateCardsPagination() {
-    const skipCards = this.paginator.page * this._cardsPerPage;
-    const topCards = this._cardsPerPage;
-    return { skipCards, topCards };
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- src/app/services/northwind.service.ts
+++ src/app/services/northwind.service.ts
@@ -1,54 +1,78 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { ProductsResult } from '../models/northwind/product-type';
-
+import { catchError, map } from 'rxjs/operators';
 
 const API_ENDPOINT = 'https://data-northwind.indigo.design';
+
 @Injectable({
   providedIn: 'root'
 })
 export class NorthwindService {
-  public remoteData: BehaviorSubject<any[]>;
-  public dataLenght: BehaviorSubject<number> = new BehaviorSubject(0);
-  public remoteCardsData: BehaviorSubject<any[]>;
-  public dataCardsLenght: BehaviorSubject<number> = new BehaviorSubject(0);
-  public url = `${API_ENDPOINT}/Products/GetPagedProducts`;
+  private remoteData = new BehaviorSubject<any[]>([]);
+  private dataLength = new BehaviorSubject<number>(0);
+  private remoteCardsData = new BehaviorSubject<any[]>([]);
+  private dataCardsLength = new BehaviorSubject<number>(0);
+  private url = `${API_ENDPOINT}/Products/GetPagedProducts`;
 
-  constructor(private http: HttpClient) {
-    this.remoteData = new BehaviorSubject([]);
-    this.remoteCardsData = new BehaviorSubject([]);
-  }
+  constructor(private http: HttpClient) {}
 
-  public getData(index?: number, perPage?: number): any {
-    let qS = '';
-
-    if (perPage) {
-      qS = `?skip=${index}&top=${perPage}`;
-    }
-    
-    this.http
-      .get<ProductsResult>(`${this.url + qS}`).pipe(
-        map((data: ProductsResult) => data)
-      ).subscribe((data) => {
+  public getData(index?: number, perPage?: number): void {
+    this.fetchData(index, perPage).subscribe(
+      data => {
         this.remoteData.next(data.items);
-        this.dataLenght.next(data.totalRecordsCount);
-      });
+        this.dataLength.next(data.totalRecordsCount);
+      },
+      error => {
+        console.error('Error fetching data', error);
+      }
+    );
   }
 
-  public getCardsData(index?: number, perPage?: number): any {
-    let qS = '';
-
-    if (perPage) {
-      qS = `?skip=${index}&top=${perPage}`;
-    }
-    
-    this.http
-      .get<ProductsResult>(`${this.url + qS}`).pipe(
-        map((data: ProductsResult) => data)
-      ).subscribe((data) => {
+  public getCardsData(index?: number, perPage?: number): void {
+    this.fetchData(index, perPage).subscribe(
+      data => {
         this.remoteCardsData.next(data.items);
-        this.dataCardsLenght.next(data.totalRecordsCount);
-      });
+        this.dataCardsLength.next(data.totalRecordsCount);
+      },
+      error => {
+        console.error('Error fetching cards data', error);
+      }
+    );
+  }
+
+  private fetchData(index?: number, perPage?: number): Observable<ProductsResult> {
+    const params: any = {};
+    if (index !== undefined) {
+      params['skip'] = index;
+    }
+    if (perPage !== undefined) {
+      params['top'] = perPage;
+    }
+
+    return this.http.get<ProductsResult>(this.url, { params }).pipe(
+      map(data => data),
+      catchError(error => {
+        console.error('Error in fetchData', error);
+        throw error;
+      })
+    );
+  }
+
+  public get remoteData$(): Observable<any[]> {
+    return this.remoteData.asObservable();
+  }
+
+  public get dataLength$(): Observable<number> {
+    return this.dataLength.asObservable();
+  }
+
+  public get remoteCardsData$(): Observable<any[]> {
+    return this.remoteCardsData.asObservable();
+  }
+
+  public get dataCardsLength$(): Observable<number> {
+    return this.dataCardsLength.asObservable();
   }
 }
